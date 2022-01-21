f6cbf109bd39103af17bca138be0175aaa44638f
takiki-web <gamr5454@gmail.com>
2022-01-21 22:06:58 +0900

    commit

---
+++ src/test/java/com/happy/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
