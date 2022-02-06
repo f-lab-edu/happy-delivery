7da0e90ef750d8f97985a851ab7e8ed5e44724e8
takiki-web <gamr5454@gmail.com>
2022-02-06 22:00:20 +0900

    feature/2-join-setup-1

--- src/main/java/com/happy/delivery/presentation/controller/UserController.java
+++ src/main/java/com/happy/delivery/presentation/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.happy.delivery.presentation.controller;
-
 import com.happy.delivery.application.dto.UserDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
-
     @PostMapping("/signup")
     public void signup(@RequestBody UserDTO userDTO) {
-
     }
 }
