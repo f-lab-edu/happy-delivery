0452f8df02cc95db6de54ad73b9dc6c863e4440c
jiae5287 <jiae5287@gmail.com>
2022-02-15 16:19:53 +0900

    SignupResult에 기본생성자 추가 및 toSignupResult()추가

--- src/main/java/com/happy/delivery/application/user/result/SignupResult.java
+++ src/main/java/com/happy/delivery/application/user/result/SignupResult.java
@@ -1,16 +1,29 @@
 package com.happy.delivery.application.user.result;
 
+import com.happy.delivery.domain.user.User;
+
 public class SignupResult {
     private String email;
     private String password;
     private String name;
     private String phoneNumber;
 
+    public SignupResult(){}
+
     public SignupResult(String email, String password, String name, String phoneNumber) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    public SignupResult toSignupResult(User user) {
+        return new SignupResult(
+                user.getEmail(),
+                user.getPassword(),
+                user.getName(),
+                user.getPhoneNumber()
+        );
     }
 
     public String getEmail() {
