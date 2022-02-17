87f2a98321696fa54257b5487422640c8e24c987
jiae5287 <jiae5287@gmail.com>
2022-02-17 21:47:06 +0900

    DeliveryApplication 필요 없는 import 삭제

--- src/main/java/com/happy/delivery/DeliveryApplication.java
+++ src/main/java/com/happy/delivery/DeliveryApplication.java
@@ -1,10 +1,7 @@
 package com.happy.delivery;
 
-import com.happy.delivery.infra.encoder.EncryptMapper;
-import com.happy.delivery.infra.encoder.JbCrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeliveryApplication {
