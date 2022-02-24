58fe8c9d98eb147591b7325ca581dcc7f29c62e1
takiki-web <gamr5454@gmail.com>
2022-02-24 16:15:53 +0900

    github action test

--- src/main/java/com/happy/delivery/DeliveryApplication.java
+++ src/main/java/com/happy/delivery/DeliveryApplication.java
@@ -2,7 +2,7 @@ package com.happy.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class DeliveryApplication {
 
