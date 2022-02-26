20985f4ebffa65b33e5f24aae3a572fcac3cee02
takiki-web <gamr5454@gmail.com>
2022-02-26 21:57:02 +0900

    메인 로직 수정

--- src/main/java/com/happy/delivery/DeliveryApplication.java
+++ src/main/java/com/happy/delivery/DeliveryApplication.java
@@ -2,12 +2,12 @@ package com.happy.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class DeliveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DeliveryApplication.class, args);
+  }
 
 }
