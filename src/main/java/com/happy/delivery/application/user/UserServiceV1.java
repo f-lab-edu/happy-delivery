f170ac90d41f0fab7d85edcf02bf2da651ca7ef4
takiki-web <gamr5454@gmail.com>
2022-02-28 20:09:25 +0900

    commit

--- src/main/java/com/happy/delivery/application/user/UserServiceV1.java
+++ src/main/java/com/happy/delivery/application/user/UserServiceV1.java
@@ -23,9 +23,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * UserServiceV1.
- */
+
 @Service
 public class UserServiceV1 implements UserService {
 
