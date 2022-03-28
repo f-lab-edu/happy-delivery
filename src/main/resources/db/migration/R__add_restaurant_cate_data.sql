c8e8913066e9cc03573008d5594956bd6a658e82
jiae5287 <jiae5287@gmail.com>
2022-03-28 18:09:31 +0900

    db update, insert data

---
+++ src/main/resources/db/migration/R__add_restaurant_cate_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO restaurant_categories (name) VALUES ('일식');
+INSERT INTO restaurant_categories (name) VALUES ('중식');
+INSERT INTO restaurant_categories (name) VALUES ('치킨');
+INSERT INTO restaurant_categories (name) VALUES ('한식');
+INSERT INTO restaurant_categories (name) VALUES ('디저트');
+INSERT INTO restaurant_categories (name) VALUES ('분식');
+INSERT INTO restaurant_categories (name) VALUES ('양식');
+INSERT INTO restaurant_categories (name) VALUES ('아시안');
+INSERT INTO restaurant_categories (name) VALUES ('패스트푸드');
