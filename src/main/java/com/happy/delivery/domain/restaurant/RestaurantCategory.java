6c7c0c7c85452cfed32da0c7f9bc0d9581df4ff7
jiae5287 <jiae5287@gmail.com>
2022-04-27 11:27:04 +0900

    @generatedValue 추가

--- src/main/java/com/happy/delivery/domain/restaurant/RestaurantCategory.java
+++ src/main/java/com/happy/delivery/domain/restaurant/RestaurantCategory.java
@@ -2,6 +2,8 @@ package com.happy.delivery.domain.restaurant;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,6 +15,7 @@ import javax.persistence.Table;
 public class RestaurantCategory {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
   @Column
