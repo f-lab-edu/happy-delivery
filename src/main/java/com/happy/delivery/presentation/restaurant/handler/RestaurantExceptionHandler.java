18a9229c19a7ad0c56ad4dcd9fc6166569755a95
jiae5287 <jiae5287@gmail.com>
2022-06-06 16:22:22 +0900

    restaurant 관련 예외 생성

--- src/main/java/com/happy/delivery/presentation/restaurant/handler/RestaurantExceptionHandler.java
+++ src/main/java/com/happy/delivery/presentation/restaurant/handler/RestaurantExceptionHandler.java
@@ -1,6 +1,8 @@
-package com.happy.delivery.presentation.user.handler;
+package com.happy.delivery.presentation.restaurant.handler;
 
 import com.happy.delivery.domain.exception.restaurant.EmptyRestaurantListException;
+import com.happy.delivery.domain.exception.restaurant.NearbyRestaurantNotExistException;
+import com.happy.delivery.domain.exception.restaurant.NoRestaurantsInThatCategoryException;
 import com.happy.delivery.presentation.common.response.ApiResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,10 +15,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestaurantExceptionHandler {
 
   /**
+   * EmptyRestaurantListException.
    * MyBatis 에서 Redis 로 가져올 음식점 데이터가 없는 경우.
    */
   @ExceptionHandler(EmptyRestaurantListException.class)
   public ApiResponse<?> emptyRestaurantListException(EmptyRestaurantListException ex) {
     return ApiResponse.fail("EMPTY_RESTAURANT_LIST", ex.getMessage());
+  }
+
+  /**
+   * NearbyRestaurantNotExistException.
+   * Redis 로 계산한 결과 근처에 배달 가능한 음식점이 없는 경우.
+   */
+  @ExceptionHandler(NearbyRestaurantNotExistException.class)
+  public ApiResponse<?> nearbyRestaurantNotExistException(NearbyRestaurantNotExistException ex) {
+    return ApiResponse.fail("NEARBY_RESTAURANT_IS_NOT_EXIST", ex.getMessage());
+  }
+
+  /**
+   * NearbyRestaurantNotExistException.
+   * Redis 로 계산한 결과 근처에 배달 가능한 음식점이 없는 경우.
+   */
+  @ExceptionHandler(NoRestaurantsInThatCategoryException.class)
+  public ApiResponse<?> noObjectsInThatCategoryException(NoRestaurantsInThatCategoryException ex) {
+    return ApiResponse.fail("NO_OBJECTS_IN_THAT_CATEGORY", ex.getMessage());
   }
 }
